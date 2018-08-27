27b6c8805a7879727086e97ff6850d860952f6ea
Mat Byczkowski <m@byczkow.ski>
2018-08-27 13:33:31 -0700

    Switch to using dep for managing dependencies

--- build.sh
+++ build.sh
@@ -25,8 +25,19 @@ ln -s "$SRCDIR" "$GOPATH/src/github.com/square/keysync"
 
 cd "$GOPATH/src/github.com/square/keysync" || exit 1
 
+# Check that dep is installed.
+if ! [ -x "$(command -v dep)" ]; then
+  echo 'dep dependency manager is missing. Installing now.'
+  go get -d -u github.com/golang/dep
+fi
+
+# Check if dependencies are up-to-date and update them otherwise.
+if [ "$(dep check -q)" != 0 ]; then
+  dep ensure -vendor-only
+fi
+
 LDFLAGS="-X main.release=$(git show --format=%H --no-patch)"
 go build -ldflags "$LDFLAGS" -o "${SRCDIR}/keysync" ./cmd/keysync
 go build -o "${SRCDIR}/keyrestore" ./cmd/keyrestore
 
-rm -r "$TMPWORK"
+rm -rf "$TMPWORK"
